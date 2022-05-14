7d006c4e4fc708cdad62d6c4fcf081f255dd7882
Your Name <shahriurahmedcse@gmail.com>
2022-05-15 02:21:34 +0600

    appointment info api created

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8ap4p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('doctors_portal').collection('services');
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello From Doctors Portal')
+})
+
+app.listen(port, () => {
+    console.log(` Doctor app listening on ${port}`)
+})
