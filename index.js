56df150a1e5666118136e9557e2fbbe3620c8502
Your Name <shahriurahmedcse@gmail.com>
2022-05-17 06:19:35 +0600

    api created for dashboard single person appointment

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ async function run() {
             res.send(services);
         });
 
+        app.get('/booking', async (req, res) => {
+            const patient = req.query.patient;
+            const query = { patient: patient };
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
         app.post('/booking', async (req, res) => {
             const booking = req.body;
             const query = { treatmentName: booking.treatmentName, date: booking.date, patientName: booking.patientName }
