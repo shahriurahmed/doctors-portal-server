d2c73491e2a1c937edb04afd7b809046bdea3f43
Md Shahriur Ahmed <tasnovakanta2000@gmail.com>
2022-05-23 12:14:12 +0600

    doctor delete system implemented

--- index.js
+++ index.js
@@ -167,6 +167,17 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
+            const doctors = await doctorCollection.find().toArray();
+            res.send(doctors);
+        })
+        app.delete('/doctor/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const result = await doctorCollection.deleteOne(filter);
+            res.send(result);
+        })
+
     }
     finally {
 
